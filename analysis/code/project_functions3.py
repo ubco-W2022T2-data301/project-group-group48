375780ee4766eca00bbc515a2933542e3254d318
ANNABELLE NGARAMBE <ngarambeannabelle@gmail.com>
2023-04-13 17:53:23 -0700

    changes to analysis 3

--- analysis/code/project_functions3.py
+++ analysis/code/project_functions3.py
@@ -1,60 +1,30 @@
 import numpy as np
 import pandas as pd
 
-def load_and_process(songdata):
-    songdata = (
-    pd.read_csv ("/Users/Annabellengarambe/Desktop/DATA_301/project-group-group48/data/raw/song_data.csv.zip") # Load data into dataframe
-    .drop_duplicates(subset= 'song_name')# Drop duplicates in the dataset
-    .loc [0:510] #Return only first 500 songs
-    )   
-  
+def clean_and_concat(songdata):
     songdata_cleaned = (
-        songdata.drop(['audio_mode', 'time_signature'],axis=1) #drop useless columns
-        .dropna(axis=0)   #drop na values
-                     
-    )
-        
-   
-    songdata_sorted = (
-        songdata.sort_values(by='song_popularity', ascending=False).reset_index() #Sort songs by popularity in descending order
-    )
+    pd.read_csv("../../data/raw/song_data.csv.zip")
+    .loc[1000:1510]
+    .drop(['audio_mode', 'time_signature'], axis=1)
+    .dropna(axis=0)
+    .drop_duplicates(subset='song_name')
+    .sort_values(by='song_popularity', ascending=False)
+    .reset_index()
+    .assign(Loud_Enrgy_Prod = lambda x: x['energy'] * x['loudness'])
+    .assign(Vale_Dance_Prod = lambda x: x['audio_valence'] * x['danceability'])
+)
     
-    
-    # This creates a new column that multiplies each loudness value to the corresponding energy value
-    def multiply_columns(row):
-        return row['energy'] * row['loudness']
-    songdata_sorted['Loud_Enrgy_Prod'] = (songdata_sorted.aggregate(multiply_columns, axis=1)) 
-
-    
-    
-    
-    
-## divide into top 15 and process top 15       
-    songdata_top = (
-        (songdata_sorted.head(20)
-               .assign(genre=['Reggae', 'Hip Hop', 'Pop', 'Pop', 'Hip Hop', 'Pop', 'Hip Hop', 
-                              'Reggae', 'Reggae', 'Pop', 'R&B', 'Pop',  'Pop', 'Pop', 'Rock', 
-                              'Pop', 'Rock', 'R&B', 'Rock', 'Rock'])
-               .reindex(columns=['song_name', 'genre', 'song_popularity', 'song_duration_ms', 
-                                 'acousticness', 'danceability', 'energy','instrumentalness',
-                                 'key', 'liveness','loudness', 'speechiness', 'tempo', 'audio_valence',
-                                 'Loud_Enrgy_Prod'])
+    concatenated_df = (
+        songdata_cleaned
+        .head(50)
+        .assign(genre=['Trap', 'Pop Rock', 'Trap', 'Hip Hop', 'Hip Hop', 'Pop', 'Pop Rock', 'Trap', 'Hip Hop', 'Trap', 'Hip Hop', 'Hip Hop', 'Hip Hop', 'Hip Hop', 'Trap', 'Pop', 'Trap', 'Hip Hop', 'Trap', 'Trap', 'Hip Hop', 'Trap', 'Hip Hop', 'Indie', 'Pop Rock', 'Pop', 'Hip Hop', 'Pop', 'Electronic', 'Acoustic Pop', 'Pop', 'Hip Hop', 'Alternative Rock', 'Pop', 'Hip Hop', 'Alternative Rock', 'Rock', 'Hip Hop', 'Pop Rock', 'Pop', 'Pop', 'Hip Hop', 'Trap', 'Pop Rock', 'Grunge', 'Hip Hop', 'Hip Hop', 'Pop', 'Alternative Rock', 'Pop Rock'])
+        .reindex(columns=['song_name', 'genre', 'song_popularity', 'song_duration_ms', 'acousticness', 'danceability', 'energy', 'instrumentalness', 'key', 'liveness', 'loudness', 'speechiness', 'tempo', 'audio_valence', 'Loud_Enrgy_Prod', 'Vale_Dance_Prod'])
+        .append(
+            songdata_cleaned
+            .tail(50)
+            .sort_values(by=['song_popularity'], ascending=True)
+            .assign(genre=['Alternative Rock', 'Hip Hop', 'Bolero', 'Latin Pop', 'Latin Pop', 'Alternative Rock', 'Bolero', 'Alternative Rock', 'Latin Pop', 'Bolero', 'Alternative Rock', 'Bolero', 'Bolero', 'Rock', 'Alternative Rock', 'Alternative Rock', 'Bolero', 'Norteño', 'Alternative Rock', 'Rock', 'Alternative Rock', 'Latin Pop', 'Rock', 'Alternative Rock', 'Pop', 'Electronic', 'Hip Hop', 'Latin Pop', 'Bachata', 'Alternative Rock', 'Pop', 'Pop', 'Norteño', 'Grunge', 'Acoustic Pop', 'Gospel', 'Bolero', 'Pop', 'Indie', 'Alternative Rock', 'Alternative Rock', 'Reggae', 'Pop', 'Indie', 'Pop', 'R&B', 'Pop Rock', 'Indie', 'Pop', 'Pop'])
+            .reindex(columns=['song_name', 'genre', 'song_popularity', 'song_duration_ms', 'acousticness', 'danceability', 'energy', 'instrumentalness', 'key', 'liveness', 'loudness', 'speechiness', 'tempo', 'audio_valence', 'Loud_Enrgy_Prod', 'Vale_Dance_Prod'])
         )
-    )
-
-## divide into bottom and process bottom 15
-    songdata_bottom = (
-        songdata_sorted.tail(20).sort_values(by=['song_popularity'], ascending = True)\
-        .assign(genre=['Latin Pop', 'Son cubano', 'Bolero', 'Hip Hop', 'Rock', 'R&B', 
-                       'Rock', 'Rock', 'Rock', 'Rock', 'Rock', 'Rock', 'Rock', 'Rock', 
-                       'Rock','Salsa', 'Rock', 'Pop', 'R&B', 'Rock'])\
-        .reindex(columns=['song_name', 'genre', 'song_popularity', 'song_duration_ms', 
-                          'acousticness', 'danceability', 'energy','instrumentalness',
-                          'key', 'liveness','loudness', 'speechiness', 'tempo', 'audio_valence', 
-                          'Loud_Enrgy_Prod'])
-    )
-    
-    
-    concatenated_df = pd.concat([songdata_top, songdata_bottom], ignore_index=True)
-    
+    ) 
     return concatenated_df
